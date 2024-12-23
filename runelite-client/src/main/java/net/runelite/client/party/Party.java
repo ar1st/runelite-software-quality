22af4e291ccbd926a10ae6bd767ce6bd5bcc567c
dimitris <dimitrischristinidis@gmail.com>
2024-12-23 22:28:12 +0200

    remove unused method

--- runelite-client/src/main/java/net/runelite/client/party/Party.java
+++ runelite-client/src/main/java/net/runelite/client/party/Party.java
@@ -13,12 +13,6 @@ package net.runelite.client.party;
       com.google.protobuf.MessageLiteOrBuilder {
 
     /**
-     * <code>int64 partyId = 1;</code>
-     * @return The partyId.
-     */
-    long getPartyId();
-
-    /**
      * <code>int64 memberId = 2;</code>
      * @return The memberId.
      */
